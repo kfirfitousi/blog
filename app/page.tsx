ac0d9a62d8f8702591f5f97ba85fcccd0cef3ffb
kfirfitousi <kfirp84@gmail.com>
2022-11-15 22:15:41 +0200

    Add support for .mdx files and example posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
-export default function Home() {
+import Link from "next/link";
+import BlogSource from "@/lib/mdx";
+
+export default async function Home() {
+  const posts = await BlogSource.getAllMdxNodes();
+
   return (
     <section className="container mx-auto pt-8 md:w-max-3xl md:pt-12 lg:pt-16">
       <div className="flex flex-col space-y-4">
         <h1 className="text-slate-800 text-center text-6xl font-bold">
           My Blog
         </h1>
+
         <h2 className="text-slate-600 text-center text-xl">
           Welcome to my personal blog
         </h2>
+
+        <div className="w-2/3 md:w-1/2 mx-auto flex flex-col space-y-4">
+          {posts.map((post) =>
+            post ? (
+              <Link key={post.slug} href={post.url}>
+                <article className="flex flex-col space-y-4 px-6 py-3 rounded bg-slate-100 hover:bg-slate-300 hover:border border-slate-800">
+                  <div className="flex flex-col space-y-2">
+                    <h2 className="max-w-[80%] text-2xl font-bold leading-normal sm:text-3xl md:text-3xl">
+                      {post.frontMatter.title}
+                    </h2>
+                    <p className="text-sm text-slate-600">
+                      {post.frontMatter.date}
+                    </p>
+                  </div>
+                  <p className="text-slate-600">{post.frontMatter.excerpt}</p>
+                  <p className="flex flex-row space-x-1">
+                    {post.frontMatter.tags.map((tag) => (
+                      <span
+                        key={tag}
+                        className="px-2 py-1 text-xs text-slate-100 bg-slate-600 rounded"
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </p>
+                </article>
+              </Link>
+            ) : null
+          )}
+        </div>
       </div>
     </section>
   );
---
+++ lib/mdx.ts
@@ -0,0 +1,152 @@
+import { promises as fs } from "fs";
+import z from "zod";
+import hasha from "hasha";
+import glob from "fast-glob";
+import path from "path";
+import NodeCache from "node-cache";
+import { VFile } from "vfile";
+import { matter } from "vfile-matter";
+
+const mdxCache = new NodeCache();
+
+export interface Source<TFrontmatter extends z.ZodType> {
+  contentPath: string;
+  basePath: string;
+  sortBy: keyof z.infer<TFrontmatter>;
+  sortOrder: "asc" | "desc";
+  frontMatter: TFrontmatter;
+}
+
+interface MdxFile {
+  filepath: string;
+  slug: string;
+  url: string;
+}
+
+interface MdxFileData<TFrontmatter extends z.ZodType> {
+  raw: string;
+  hash: string;
+  frontMatter: TFrontmatter;
+  content: string;
+}
+
+export function createMdxSource<TFrontmatter extends z.ZodType>(
+  source: Source<TFrontmatter>
+) {
+  const { contentPath, basePath, sortBy, sortOrder } = source;
+
+  async function getMdxFiles() {
+    const files = await glob(`${contentPath}/**/*.{md,mdx}`);
+
+    if (!files.length) return [];
+
+    return files.map((filepath) => {
+      let slug = filepath
+        .replace(contentPath, "")
+        .replace(/^\/+/, "")
+        .replace(new RegExp(path.extname(filepath) + "$"), "");
+
+      slug = slug.replace(/\/?index$/, "");
+
+      return {
+        filepath,
+        slug,
+        url: `${basePath?.replace(/\/$/, "")}/${slug}`,
+      };
+    });
+  }
+
+  async function getFileData(
+    file: MdxFile
+  ): Promise<MdxFileData<z.infer<TFrontmatter>>> {
+    const raw = await fs.readFile(file.filepath, "utf-8");
+    const hash = hasha(raw.toString());
+
+    const cachedContent =
+      mdxCache.get<MdxFileData<z.infer<TFrontmatter>>>(hash);
+    if (cachedContent?.hash === hash) {
+      return cachedContent;
+    }
+
+    const vfile = new VFile({ value: raw });
+    matter(vfile, { strip: true });
+
+    const frontMatter = source.frontMatter.parse(
+      vfile.data.matter
+    ) as z.infer<TFrontmatter>;
+
+    const fileData = {
+      raw,
+      frontMatter,
+      hash,
+      content: String(vfile.value),
+    };
+
+    mdxCache.set(hash, fileData);
+
+    return fileData;
+  }
+
+  async function getMdxNode(slug: string | string[]) {
+    const _slug = Array.isArray(slug) ? slug.join("/") : slug;
+
+    const files = await getMdxFiles();
+    if (!files?.length) return null;
+
+    const [file] = files.filter((file) => file.slug === _slug);
+    if (!file) return null;
+
+    const data = await getFileData(file);
+
+    return {
+      ...file,
+      ...data,
+    };
+  }
+
+  async function getAllMdxNodes() {
+    const files = await getMdxFiles();
+
+    if (!files.length) return [];
+
+    const nodes = await Promise.all(
+      files.map(async (file) => {
+        return await getMdxNode(file.slug);
+      })
+    );
+
+    const adjust = sortOrder === "desc" ? -1 : 1;
+    return nodes.sort((a, b) => {
+      if (!a || !b) return 0;
+      if (a.frontMatter[sortBy] < b.frontMatter[sortBy]) {
+        return -1 * adjust;
+      }
+      if (a.frontMatter[sortBy] > b.frontMatter[sortBy]) {
+        return 1 * adjust;
+      }
+      return 0;
+    });
+  }
+
+  return {
+    getMdxFiles,
+    getFileData,
+    getMdxNode,
+    getAllMdxNodes,
+  };
+}
+
+const BlogSource = createMdxSource({
+  contentPath: "content/blog",
+  basePath: "/blog",
+  sortBy: "date",
+  sortOrder: "desc",
+  frontMatter: z.object({
+    title: z.string(),
+    date: z.string(),
+    excerpt: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export default BlogSource;
