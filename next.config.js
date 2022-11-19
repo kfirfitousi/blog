e1b7b37788a75325c416d6b4459b3832fded95ed
kfirfitousi <kfirp84@gmail.com>
2022-11-19 19:19:28 +0200

    Add redirect: /blog -> /

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
