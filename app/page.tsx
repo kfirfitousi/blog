7833c42b0f2a9c6f7de8abf58d1af821302b3f21
kfirfitousi <kfirp84@gmail.com>
2022-11-20 13:37:51 +0200

    Give title icons min-width

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default async function Home() {
   return (
     <div className="flex flex-col space-y-4 px-6 sm:px-12 pb-12">
       <section className="col-span-3 inline-flex items-center justify-center pb-8 md:pb-12">
-        <Waves className="w-8 h-8 text-rose-700 dark:text-rose-50 text-opacity-40" />
-        <h1 className="pb-1.5 mx-2 text-slate-800 dark:text-rose-50 text-center text-6xl font-bold drop-shadow-sm whitespace-nowrap">
+        <Waves className="w-8 h-8 min-w-max text-rose-700 dark:text-rose-50 text-opacity-40" />
+        <h1 className="pb-1.5 mx-2 text-slate-800 dark:text-rose-50 text-center text-5xl sm:text-6xl font-bold drop-shadow-sm whitespace-nowrap">
           Kfir&apos;s Blog
         </h1>
-        <Waves className="w-8 h-8 text-rose-700 dark:text-rose-50 text-opacity-40" />
+        <Waves className="w-8 h-8 min-w-max text-rose-700 dark:text-rose-50 text-opacity-40" />
       </section>
       <section className="w-full col-start-2 flex flex-col space-y-4">
         {posts.map((post) => (
