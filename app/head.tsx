41ae9f980fb601ed9b3f1b4dcc6abdc79bb52306
kfirfitousi <kfirp84@gmail.com>
2022-11-21 11:23:57 +0200

    Update meta description

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,10 @@ export default function Head() {
     <>
       <title>Kfir&apos;s Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="My personal blog." />
+      <meta
+        name="description"
+        content="My name is Kfir and I'm a Full Stack developer. Welcome to my personal dev blog!"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
