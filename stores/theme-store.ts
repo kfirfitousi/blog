d0b86c2405134eb427dceee8c288647b5d4211a0
kfirfitousi <kfirp84@gmail.com>
2022-11-24 20:01:28 +0200

    Fix font size increase/decrease actions

--- stores/theme-store.ts
+++ stores/theme-store.ts
@@ -40,6 +40,7 @@ export const useThemeStore = create<ThemeState>()(
           : {
               fontSize: fontSizes[index + 1],
               isFontSizeMax: index === fontSizes.length - 2,
+              isFontSizeMin: false,
             };
       }),
     decreaseFontSize: () =>
@@ -50,6 +51,7 @@ export const useThemeStore = create<ThemeState>()(
           : {
               fontSize: fontSizes[index - 1],
               isFontSizeMin: index === 1,
+              isFontSizeMax: false,
             };
       }),
   }))
