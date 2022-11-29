0fad19dbf4e854247acd2cfcffa38824a504af1c
kfirfitousi <kfirp84@gmail.com>
2022-11-29 15:03:46 +0200

    Fix post tags layout

--- components/post-tags.tsx
+++ components/post-tags.tsx
@@ -7,11 +7,11 @@ interface PostTagsProps {
 
 export function PostTags({ tags, className }: PostTagsProps) {
   return (
-    <p className={clsx(className, "flex flex-row space-x-1")}>
+    <p className={clsx(className, "flex flex-row gap-1 flex-wrap")}>
       {tags.map((tag) => (
         <span
           key={tag}
-          className="w-fit px-2 py-1 text-slate-100 dark:text-slate-900 bg-slate-600 dark:bg-rose-50 rounded"
+          className="w-fit px-2 py-1 text-slate-100 dark:text-slate-900 bg-slate-600 dark:bg-rose-50 rounded whitespace-nowrap"
         >
           {tag}
         </span>
