4820d27de2eec7eed90ca240d413e347542ac69d
kfirfitousi <kfirp84@gmail.com>
2022-12-04 20:29:32 +0200

    Fix wrong className

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -18,7 +18,7 @@ export function MdxContent({ source }: MdxContentProps) {
       className={clsx(
         isSerif && "font-serif",
         "prose-" + fontSize,
-        "dark:prose-blockquote:text-rose-50dark:prose-li:marker:text-slate-400 prose prose-slate max-w-none py-8 prose-headings:drop-shadow-sm prose-a:text-rose-700 prose-a:no-underline prose-blockquote:border-l-slate-800 prose-ul:my-1 prose-li:my-0 prose-li:marker:text-slate-600 prose-hr:border-slate-700 dark:prose-invert dark:text-rose-50 dark:prose-headings:text-rose-50 dark:prose-a:text-rose-300 dark:prose-blockquote:border-l-slate-300 dark:prose-hr:border-slate-300"
+        "prose prose-slate max-w-none py-8 prose-headings:drop-shadow-sm prose-a:text-rose-700 prose-a:no-underline prose-blockquote:border-l-slate-800 prose-ul:my-1 prose-li:my-0 prose-li:marker:text-slate-600 prose-hr:border-slate-700 dark:prose-invert dark:text-rose-50 dark:prose-headings:text-rose-50 dark:prose-a:text-rose-300 dark:prose-blockquote:border-l-slate-300 dark:prose-blockquote:text-rose-50 dark:prose-li:marker:text-slate-400 dark:prose-hr:border-slate-300"
       )}
     >
       <MDXRemote {...source} components={MdxComponents} />
