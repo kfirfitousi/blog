4613f232a07dc3ba4f6057b4d96a0c0c27d51266
kfirfitousi <kfirp84@gmail.com>
2022-12-12 15:20:06 +0200

    Add twitter card meta tags

--- app/[...slug]/head.tsx
+++ app/[...slug]/head.tsx
@@ -19,14 +19,16 @@ export default async function PostHead({ params }: PostHeadProps) {
       <title>{`Kfir's Blog | ${title}`}</title>
       <meta name="description" content={excerpt} />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={excerpt} />
+      <meta property="og:type" content="article" />
       <meta
         property="og:image"
         content={`https://blog.kfirfitousi.com/api/og?title=${title}&subtitle=${excerpt}`}
       />
-      <meta
-        name="twitter:image"
-        content={`https://blog.kfirfitousi.com/api/og?title=${title}&subtitle=${excerpt}`}
-      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@kp2c" />
+      <meta name="twitter:creator" content="@kp2c" />
     </>
   );
 }
--- app/head.tsx
+++ app/head.tsx
@@ -2,13 +2,21 @@ export default function Head() {
   return (
     <>
       <title>Kfir&apos;s Blog</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
         content="My name is Kfir and I'm a Full Stack developer. Welcome to my personal dev blog!"
       />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="og:title" content="Kfir's Blog" />
+      <meta
+        property="og:description"
+        content="My name is Kfir and I'm a Full Stack developer. Welcome to my personal dev blog!"
+      />
       <meta property="og:image" content="https://blog.kfirfitousi.com/api/og" />
-      <meta name="twitter:image" content="https://blog.kfirfitousi.com/api/og" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@kp2c" />
+      <meta name="twitter:creator" content="@kp2c" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
