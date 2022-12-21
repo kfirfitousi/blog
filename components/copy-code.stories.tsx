633a2538f08950d6ac01bf914255860e1f3d42f7
kfirfitousi <kfirp84@gmail.com>
2022-12-21 22:20:07 +0200

    Add copy-code story

---
+++ components/copy-code.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CopyCode } from './copy-code';
+import '@/styles/markdown.css';
+
+const codeElement = (
+  <code className="hljs rounded">
+    {`
+import type { Meta, StoryObj } from '@storybook/react';
+import { CopyCode } from './copy-code';
+import '@/styles/markdown.css';
+    `}
+  </code>
+);
+
+const meta: Meta<typeof CopyCode> = {
+  title: 'Copy Code Button',
+  component: CopyCode,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <pre className="relative mx-auto max-w-3xl">
+        <Story args={{ codeElement }} />
+        {codeElement}
+      </pre>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof CopyCode>;
+
+export const Normal: Story = {};
