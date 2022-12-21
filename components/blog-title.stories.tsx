78f62f90585cfa558580053ca9646c297ddd9026
kfirfitousi <kfirp84@gmail.com>
2022-12-21 21:32:23 +0200

    Add blog-title story

---
+++ components/blog-title.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BlogTitle } from './blog-title';
+
+const meta: Meta<typeof BlogTitle> = {
+  title: 'Blog Title',
+  component: BlogTitle,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof BlogTitle>;
+
+export const Big: Story = {
+  args: {
+    title: "Kfir's Blog",
+    small: false,
+  },
+};
+
+export const Small: Story = {
+  args: {
+    title: "Kfir's Blog",
+    small: true,
+  },
+};
