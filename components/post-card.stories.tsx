c15f41f094ff8bcbb43d3ce42cc131b1fc88850e
kfirfitousi <kfirp84@gmail.com>
2022-12-21 22:47:07 +0200

    Add post-card story

---
+++ components/post-card.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PostCard } from './post-card';
+
+const meta: Meta<typeof PostCard> = {
+  title: 'Post Card',
+  component: PostCard,
+  decorators: [
+    (Story) => (
+      <div className="mx-auto flex max-w-3xl">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof PostCard>;
+
+export const Normal: Story = {
+  args: {
+    post: {
+      frontmatter: {
+        title: 'Example Post',
+        date: '2022-01-01',
+        tags: ['example', 'post'],
+        excerpt: 'This is an example post.',
+      },
+      slug: 'example-post',
+      url: '/posts/examples/example-post',
+      filepath: 'posts/examples/example-post.mdx',
+      hash: 'example-post',
+      raw: '',
+      serialized: '' as any,
+    },
+  },
+};
+
+export const FreshPost: Story = {
+  args: {
+    post: {
+      frontmatter: {
+        title: 'Example Post',
+        date: new Date().toISOString(),
+        tags: ['example', 'post'],
+        excerpt: 'This is an example post.',
+      },
+      slug: 'example-post',
+      url: '/posts/examples/example-post',
+      filepath: 'posts/examples/example-post.mdx',
+      hash: 'example-post',
+      raw: '',
+      serialized: '' as any,
+    },
+  },
+};
