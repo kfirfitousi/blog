f4a22c9f68ababbfa17b43dd7ac4cd75fc415b9e
kfirfitousi <kfirp84@gmail.com>
2022-12-23 13:10:21 +0200

    Install react-tooltip and create tooltip component

---
+++ components/tooltip.tsx
@@ -0,0 +1,11 @@
+import { Tooltip as ReactTooltip, ITooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
+export function Tooltip(props: ITooltip) {
+  return (
+    <ReactTooltip
+      className="bg-slate-700 text-sm text-slate-300 dark:bg-slate-300 dark:text-slate-700"
+      {...props}
+    />
+  );
+}
