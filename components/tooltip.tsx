179472b65983c22a948771927450c583962e171d
kfirfitousi <kfirp84@gmail.com>
2022-12-23 13:17:58 +0200

    Fix tooltip component style

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -4,7 +4,7 @@ import 'react-tooltip/dist/react-tooltip.css';
 export function Tooltip(props: ITooltip) {
   return (
     <ReactTooltip
-      className="bg-slate-700 text-sm text-slate-300 dark:bg-slate-300 dark:text-slate-700"
+      className="!bg-slate-700 text-sm !text-slate-200 dark:!bg-slate-200 dark:!text-slate-700"
       {...props}
     />
   );
