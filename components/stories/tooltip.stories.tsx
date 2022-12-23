61283337ac37825736b4c574398aa578e83710ff
kfirfitousi <kfirp84@gmail.com>
2022-12-23 13:54:18 +0200

    Add tooltip story

---
+++ components/stories/tooltip.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tooltip } from '@/components/tooltip';
+import { Github } from 'lucide-react';
+import { Center } from './decorators';
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Tooltip',
+  component: Tooltip,
+  decorators: [
+    (Story, ctx) => (
+      <>
+        <Github
+          id="github"
+          className="h-6 w-6 text-slate-700 hover:scale-110 hover:pb-0.5 hover:text-rose-600 dark:text-rose-50 dark:hover:text-rose-400"
+          data-tooltip-content={ctx.args.content}
+          aria-label={ctx.args.content}
+        />
+        <Story />
+      </>
+    ),
+    Center,
+  ],
+  argTypes: {
+    anchorId: {
+      control: false,
+      defaultValue: 'github',
+    },
+    content: {
+      control: 'text',
+      defaultValue: 'My GitHub profile',
+    },
+    place: {
+      control: false,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Tooltip>;
+
+export const Top: Story = { args: { place: 'top' } };
+
+export const Bottom: Story = { args: { place: 'bottom' } };
+
+export const Left: Story = { args: { place: 'left' } };
+
+export const Right: Story = { args: { place: 'right' } };
