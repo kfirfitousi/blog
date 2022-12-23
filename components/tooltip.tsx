b5287cd760d0de2d0d4e59a43604370885cab15c
kfirfitousi <kfirp84@gmail.com>
2022-12-23 14:51:48 +0200

    Hide tooltips on mobile

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -4,7 +4,7 @@ import 'react-tooltip/dist/react-tooltip.css';
 export function Tooltip(props: ITooltip) {
   return (
     <ReactTooltip
-      className="!bg-slate-700 text-sm !text-slate-200 dark:!bg-slate-200 dark:!text-slate-700"
+      className="hidden !bg-slate-700 text-sm !text-slate-200 dark:!bg-slate-200 dark:!text-slate-700 sm:block"
       {...props}
     />
   );
