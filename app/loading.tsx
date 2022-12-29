c464cf916815646230e5d53c20c018339b59057b
kfirfitousi <kfirp84@gmail.com>
2022-12-29 22:52:34 +0200

    Fix loading spinner style

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 export default function Loading() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <Loader2 className="h-12 w-12 animate-spin" />
+      <Loader2 className="h-12 w-12 animate-spin text-slate-700 dark:text-rose-50" />
     </div>
   );
 }
