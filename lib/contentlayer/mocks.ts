9a60ecb3773ca2cac8b3603fcbc8e04d3891cce0
kfirfitousi <kfirp84@gmail.com>
2023-01-11 00:01:04 +0200

    Tweak mocks

--- lib/contentlayer/mocks.ts
+++ lib/contentlayer/mocks.ts
@@ -1,17 +1,17 @@
-import type { Document, MDX } from 'contentlayer/dist/core';
+import type { DocumentMeta, MDX } from 'contentlayer/dist/core';
 
-type MdxDocument = Document & {
+interface MdxDocument extends DocumentMeta {
+  [key: string]: any;
   body: MDX;
   type: string;
-};
+}
 
 type Fields<D extends MdxDocument> = Omit<D, '_id' | '_raw' | 'body' | 'type'>;
 
 /**
- * Generates a dummy MDX document with the given fields, matching the given type.
+ * Generates a dummy MDX document with the given fields and properties, matching the given type.
  * @param fields - The fields to set on the dummy document.
- * @param id - The id of the dummy document. Defaults to 'dummy'.
- * @param type - The type of the dummy document. Defaults to 'Post'.
+ * @param properties - The properties to set on the dummy document.
  * @example
  * import { type Post } from 'contentlayer/generated';
  *
@@ -19,51 +19,67 @@ type Fields<D extends MdxDocument> = Omit<D, '_id' | '_raw' | 'body' | 'type'>;
  *   {
  *     title: 'Example Post',
  *     date: '2023-01-01',
+ *     slug: 'example-post',
  *   },
- *   'example-post',
- *   'Post'
+ *   {
+ *     _id: 'example-post',
+ *     flattenedPath: 'posts/example-post',
+ *   }
  * );
  */
 export function dummy<D extends MdxDocument>(
   fields: Fields<D>,
-  id = 'dummy',
-  type = 'Post',
-): D {
+  properties?: Partial<D>,
+) {
   return {
-    _id: id,
-    _raw: {},
+    _id: properties?._id || 'dummy',
+    _raw: {
+      contentType: 'mdx',
+      flattenedPath: 'posts/dummy',
+      sourceFileDir: 'posts',
+      sourceFileName: 'dummy-post.mdx',
+      sourceFilePath: 'posts/dummy-post.mdx',
+      ...properties?._raw,
+    } as D['_raw'],
     body: {
       code: '',
       raw: 'dummy document raw content',
+      ...properties?.body,
     },
-    type,
+    type: properties?.type || 'dummy',
     ...fields,
   } as D;
 }
 
 /**
- * Generates an array of dummy MDX documents with the given fields, matching the given type.
- * @param count - The number of dummy documents to generate. Defaults to 1.
+ * Generates an array of dummy MDX documents with the given fields and properties, matching the given type.
+ * @param count - The number of dummy documents to generate.
  * @param fields - The fields to set on the dummy documents. Can be a function that returns the fields for each document.
- * @param type - The type of the dummy documents. Defaults to 'Post'.
+ * @param properties - The properties to set on the dummy documents. Can be a function that returns the properties for each document.
  * @example
  * import { type Post } from 'contentlayer/generated';
  *
- * const posts = dummies<Post>(10, (index) => ({
- *   title: `Post ${index + 1}`,
- *   date: '2023-01-01',
- * }));
+ * const posts = dummies<Post>(
+ *   10,
+ *   (index) => ({
+ *     title: `Post ${index + 1}`,
+ *     date: '2022-01-01',
+ *   }),
+ *   (index) => ({
+ *     _id: `post-${index + 1}`,
+ *     raw: 'raw content',
+ *   }),
+ * );
  */
 export function dummies<D extends MdxDocument>(
-  count = 1,
-  fields: ((index: number) => Fields<D>) | Fields<D>,
-  type = 'Post',
+  count: number,
+  fields: Fields<D> | ((index: number) => Fields<D>),
+  properties?: Partial<D> | ((index: number) => Partial<D>),
 ) {
   return Array.from({ length: count }, (_, index) => {
-    if (typeof fields === 'function') {
-      return dummy(fields(index), index.toString(), type);
-    }
-
-    return dummy(fields);
+    return dummy(
+      typeof fields === 'function' ? fields(index) : fields,
+      typeof properties === 'function' ? properties(index) : properties,
+    );
   });
 }
