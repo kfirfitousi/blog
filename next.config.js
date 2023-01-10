79e3a09fcf3e7ed24a67a91b13fbc1790d009a8b
kfirfitousi <kfirp84@gmail.com>
2023-01-10 20:18:14 +0200

    Convert next config to cjs (fixes webpack issue)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from 'next-contentlayer';
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -23,4 +23,4 @@ const nextConfig = {
   },
 };
 
-export default withContentlayer(nextConfig);
+module.exports = withContentlayer(nextConfig);
