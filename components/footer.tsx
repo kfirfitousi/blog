05b349012fc564f0d109d23f08b5941f154cfc2e
kfirfitousi <kfirp84@gmail.com>
2023-01-15 19:08:34 +0200

    Add 'buy me a pizza' button to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,25 @@
 'use client';
 
-import { AtSign, Copyright, Github, Linkedin, Twitter } from 'lucide-react';
 import { Tooltip } from '@/components/tooltip';
 import { cn } from '@/lib/utils';
-import Link from 'next/link';
+import {
+  AtSign,
+  Copyright,
+  Github,
+  Linkedin,
+  Pizza,
+  Twitter,
+} from 'lucide-react';
 
 export function Footer() {
   return (
     <footer className="relative grid h-full w-full grid-rows-2 flex-col items-center justify-center py-4">
       <div className="flex flex-row space-x-6">
-        <Link href="https://www.github.com/kfirfitousi" target="_blank">
+        <a
+          href="https://www.github.com/kfirfitousi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Github
             id="github"
             className="icon-base"
@@ -17,11 +27,12 @@ export function Footer() {
             aria-label="My GitHub profile"
           />
           <Tooltip anchorId="github" />
-        </Link>
-        <Link
+        </a>
+        <a
           href="https://www.twitter.com/kp2c"
           target="_blank"
           className="ml-6"
+          rel="noopener noreferrer"
         >
           <Twitter
             id="twitter"
@@ -30,7 +41,7 @@ export function Footer() {
             aria-label="My Twitter profile"
           />
           <Tooltip anchorId="twitter" />
-        </Link>
+        </a>
         <a href="mailto:kfirfitousi@gmail.com" className="ml-6">
           <AtSign
             id="email"
@@ -40,9 +51,10 @@ export function Footer() {
           />
           <Tooltip anchorId="email" />
         </a>
-        <Link
+        <a
           href="https://www.linkedin.com/in/kfirp"
           target="_blank"
+          rel="noopener noreferrer"
           className="ml-6"
         >
           <Linkedin
@@ -52,7 +64,7 @@ export function Footer() {
             aria-label="My LinkedIn profile"
           />
           <Tooltip anchorId="linkedin" />
-        </Link>
+        </a>
         <a
           href="https://story.blog.kfirfitousi.com"
           target="_blank"
@@ -68,7 +80,7 @@ export function Footer() {
             role="img"
             fill="none"
             stroke="currentColor"
-            strokeWidth="2.4"
+            strokeWidth="2.6"
             className="icon-base"
             data-tooltip-content="Storybook"
             aria-label="Storybook"
@@ -76,6 +88,20 @@ export function Footer() {
             <path d="M21.786 0.318l-0.161 3.615c-0.005 0.203 0.229 0.328 0.391 0.203l1.411-1.068 1.198 0.932c0.156 0.104 0.365 0 0.375-0.188l-0.135-3.677 1.776-0.135c0.922-0.063 1.708 0.672 1.708 1.599v28.802c0 0.917-0.766 1.646-1.682 1.599l-21.469-0.958c-0.833-0.036-1.505-0.708-1.531-1.547l-1-26.401c-0.052-0.885 0.62-1.646 1.505-1.693l17.599-1.109zM17.693 12.401c0 0.625 4.214 0.318 4.786-0.109 0-4.266-2.292-6.521-6.479-6.521-4.198 0-6.531 2.297-6.531 5.724 0 5.932 8 6.036 8 9.276 0 0.938-0.427 1.469-1.401 1.469-1.281 0-1.802-0.651-1.734-2.88 0-0.479-4.865-0.641-5.026 0-0.359 5.375 2.974 6.932 6.797 6.932 3.724 0 6.63-1.984 6.63-5.573 0-6.359-8.135-6.188-8.135-9.333 0-1.292 0.964-1.464 1.505-1.464 0.604 0 1.667 0.094 1.589 2.49z" />
           </svg>
           <Tooltip anchorId="storybook" />
+        </a>
+        <a
+          href="https://www.buymeacoffee.com/kfirfitousi"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-6"
+        >
+          <Pizza
+            id="pizza"
+            className="icon-base"
+            data-tooltip-content="Buy me a pizza"
+            aria-label="Buy me a pizza"
+          />
+          <Tooltip anchorId="pizza" />
         </a>
       </div>
       <div
