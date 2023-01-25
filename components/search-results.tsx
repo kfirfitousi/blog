a2f438706c409eef7b2052f689c58292c7676b95
kfirfitousi <kfirp84@gmail.com>
2023-01-25 12:34:21 +0200

    Refactor search results

--- components/search-results.tsx
+++ components/search-results.tsx
@@ -5,6 +5,7 @@ import { useSearchStore } from '@/stores/search-store';
 import { formatDateTime } from '@/lib/datetime';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import Balancer from 'react-wrap-balancer';
 
 type SearchResultsProps = {
   query: string;
@@ -20,26 +21,30 @@ export function SearchResults({ query, results }: SearchResultsProps) {
         const dateTime = formatDateTime(post.date);
 
         return (
-          <Link
+          <li
             key={post.slug}
-            href={post.url}
-            onClick={toggleSearch}
             className={cn(
-              'flex h-fit flex-col rounded p-2 transition-none sm:px-8',
+              'rounded p-2 transition-none sm:px-8',
               'even:bg-slate-400/30 hover:bg-slate-500/50',
               'dark:even:bg-slate-700/60 dark:hover:bg-slate-400/40',
             )}
           >
-            <span className="font-semibold text-slate-800 dark:text-rose-50 sm:text-xl">
-              {highlightSearchQuery(query, post.title)}
-            </span>
-            <span className="text-sm text-slate-700 dark:text-rose-50 sm:text-base">
-              {highlightSearchQuery(query, post.excerpt)}
-            </span>
-            <span className="text-sm text-slate-600 dark:text-slate-300">
-              {dateTime.asString} · {dateTime.asRelativeTimeString}
-            </span>
-          </Link>
+            <Link
+              href={post.url}
+              onClick={toggleSearch}
+              className="flex h-fit flex-col"
+            >
+              <span className="font-semibold text-slate-800 dark:text-rose-50 sm:text-xl">
+                <Balancer>{highlightSearchQuery(query, post.title)}</Balancer>
+              </span>
+              <span className="text-sm text-slate-700 dark:text-rose-50 sm:text-base">
+                {highlightSearchQuery(query, post.excerpt)}
+              </span>
+              <span className="text-sm text-slate-600 dark:text-slate-300">
+                {dateTime.asString} · {dateTime.asRelativeTimeString}
+              </span>
+            </Link>
+          </li>
         );
       })}
     </ul>
