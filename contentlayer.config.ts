c0cf01817f33666f5747710b806221de0a82d3eb
kfirfitousi <kfirp84@gmail.com>
2023-02-09 17:32:20 +0200

    Improve syntax highlighting

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,8 +3,17 @@ import {
   makeSource,
   type ComputedFields,
 } from 'contentlayer/source-files';
+import { s } from 'hastscript';
+import rehypeAutolinkHeadings, {
+  type Options as AutolinkOptions,
+} from 'rehype-autolink-headings';
+import rehypePrettyCode, {
+  type Options as PrettyCodeOptions,
+} from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 
-import { rehypePlugins, remarkPlugins } from './lib/mdx-plugins';
+import { blogConfig } from './config';
 
 const computedFields: ComputedFields = {
   slug: {
@@ -79,7 +88,65 @@ export default makeSource({
   contentDirPath: './content',
   documentTypes: [Post, Page],
   mdx: {
-    remarkPlugins,
-    rehypePlugins,
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          test: ['h1', 'h2', 'h3'],
+          content: s(
+            'svg',
+            {
+              xmlns: 'http://www.w3.org/2000/svg',
+              viewBox: '0 0 24 24',
+              width: '24',
+              height: '24',
+              fill: 'none',
+              stroke: 'currentColor',
+              'stroke-width': '2',
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'aria-label': 'Anchor link',
+            },
+            [
+              s('line', { x1: '4', y1: '9', x2: '20', y2: '9' }),
+              s('line', { x1: '4', y1: '15', x2: '20', y2: '15' }),
+              s('line', { x1: '10', y1: '3', x2: '8', y2: '21' }),
+              s('line', { x1: '16', y1: '3', x2: '14', y2: '21' }),
+            ],
+          ),
+        } satisfies Partial<AutolinkOptions>,
+      ],
+      [
+        rehypePrettyCode,
+        {
+          theme: {
+            light: blogConfig.theme?.codeBlockTheme?.light || 'github-light',
+            dark: blogConfig.theme?.codeBlockTheme?.dark || 'github-dark',
+          },
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and
+            // allow empty lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push('highlighted');
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ['word'];
+          },
+          tokensMap: {
+            fn: 'entity.name',
+            type: 'entity.name',
+            prop: 'entity.name',
+            const: 'variable.other.constant',
+          },
+        } satisfies Partial<PrettyCodeOptions>,
+      ],
+    ],
   },
 });
